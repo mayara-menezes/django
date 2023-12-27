71da775ecbca16b0b08a2f1d516e1864372d7db7
mayara-menezes <79277369+mayara-menezes@users.noreply.github.com>
2023-12-27 00:33:35 -0300

    Delete initial migration and add code to create a new album

--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+# from galeria.models import Album
+
+# foto = Album()
+# foto.nome = "Nebulosa de Carina"
+# foto.legenda = "Nebulosa de Carina"
+# foto.descricao = "A Nebulosa de Carina é uma grande nebulosa de emissão localizada na constelação de Carina. A nebulosa contém várias estrelas de Wolf-Rayet, que são estrelas massivas na fase final de suas vidas. A nebulosa é um dos maiores berçários de estrelas da Via Láctea."
+# foto.foto = "carina-nebula.png"
+# foto.save()
 
 def index(request):
     return render(request, 'galeria/index.html')
